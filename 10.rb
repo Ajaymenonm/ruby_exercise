b3e2805f056dcf1d8aa0627a5acf583246b99467
Ajay Menon <ammuralidhar@qwinixtech.com>
2015-07-31 18:21:36 +0530

    added prog 9 & 10

---
+++ 10.rb
@@ -0,0 +1,14 @@
+puts "enter the week"
+a=gets.chomp
+case a
+when "monday"
+  puts "It's monday"
+when "tuesday"
+  puts "It's tuesday"
+  when "wednesday"
+  puts "It's wednesday"
+when "thursday"
+  puts "It's thursday "
+else
+  puts "This is not the day of week"
+end
--- 9.rb
+++ 9.rb
@@ -1,6 +1,15 @@
-x = 1
-while x <= 10
-   puts "#{x} * 1 = #{x} , #{x} * 2 = #{x*2}, #{x} * 3 = #{x*3}......
-#{x} * 1 = #{x*10}"
-   x = x + 1
+# x = 1
+# while x<=10
+#  (1..5).each do |n|
+#    puts "\n" if n ==1
+#    print "#{x}*#{n}=#{x*n}"+"\t"
+#  end
+#  x += 1
+# end
+
+(1..5).each do |x|
+    # puts "x - " + x.to_s
+  (1..10).each do |n|
+    puts "#{x} * #{n} = #{x * n}"
+  end
 end
